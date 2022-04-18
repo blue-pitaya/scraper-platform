2a0a117e1367cdf560405c0e0b70b85a9dd17a2e
blue-pitaya <blue.pitaya@pm.me>
2022-04-19 01:47:53 +0200

    Moved CsvParser type class to seperate file.

---
+++ src/main/scala/example/CsvParser.scala
@@ -0,0 +1,12 @@
+package example
+
+trait CsvParser[A] {
+  def parse(value: A): List[List[String]]
+}
+
+object CsvParser {
+  implicit val stringSaver = new CsvParser[List[String]] {
+    override def parse(value: List[String]): List[List[String]] = List(value)
+  }
+}
+
